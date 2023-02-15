56a98a9b674f1a5a5270aa746ac663af822e4b08
resix237 <resix237pro@gmail.com>
2023-02-15 16:06:11 +0100

    intialisation

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
+      <Text>hello tes et jes suis la </Text>
       <StatusBar style="auto" />
     </View>
   );
